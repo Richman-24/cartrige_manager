cbf863e43bb4bb7cf9e2b6c656038a73b794172d
MaxDDaOS <dreev24@yandex.ru>
2025-05-12 15:03:35 +0300

    0.06 - Добавил экран кабинета в кампусе, теперь отображается список устройств в кабинете

--- CMan/buildings/urls.py
+++ CMan/buildings/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from buildings.views import CampusListView
+from buildings.views import CampusListView, CampusCabinetView
 
 app_name="campus"
 
 
 urlpatterns = [
     path('', CampusListView.as_view(), name="index"),
+    path('<int:cabinet_pk>/', CampusCabinetView.as_view(), name="cabinet"),
+
 ]
--- CMan/buildings/views.py
+++ CMan/buildings/views.py
@@ -1,9 +1,27 @@
-from django.views.generic import ListView
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 
 from buildings.models import Cabinet
+from devices.models import UsablePrinter
 
 
 class CampusListView(ListView):
     model = Cabinet
     context_object_name = "cabinets"
 
+
+class CampusCabinetView(DetailView):
+    model = Cabinet
+    template_name = "buildings/cabinet_detail.html"
+    pk_url_kwarg = "cabinet_pk"
+    context_object_name = "devices"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cabinet_pk = self.kwargs.get(self.pk_url_kwarg)
+        context["devices"] = (
+            UsablePrinter.objects.select_related()
+            .filter(cabinet_id=cabinet_pk)
+        )
+        
+        return context
